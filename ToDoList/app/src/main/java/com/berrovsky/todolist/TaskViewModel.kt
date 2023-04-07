2f88f20637b80e1b28adf6bb2dc52cf11c4bf3e7
berraozkan <berraozkan00@gmail.com>
2023-04-07 22:32:15 +0300

    Todo List App

---
+++ ToDoList/app/src/main/java/com/berrovsky/todolist/TaskViewModel.kt
@@ -0,0 +1,10 @@
+package com.berrovsky.todolist
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel: ViewModel()
+{
+    var name = MutableLiveData<String>()
+    var desc = MutableLiveData<String>()
+}
